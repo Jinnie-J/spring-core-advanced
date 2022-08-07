fc3fa256f2856c9a4375c3d7ea3109eb204fcd5e
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-08-07 22:09:27 +0900

    템플릿 메서드 패턴 - 적용, 정의

---
+++ src/main/java/jinnie/advanced/trace/template/AbstractTemplate.java
@@ -0,0 +1,32 @@
+package jinnie.advanced.trace.template;
+
+import jinnie.advanced.trace.TraceStatus;
+import jinnie.advanced.trace.logtrace.LogTrace;
+
+public abstract class AbstractTemplate<T> {
+    
+    private final LogTrace trace;
+    
+    public AbstractTemplate(LogTrace trace){
+        this.trace= trace;
+    }
+    
+    public T execute(String message){
+        TraceStatus status = null;
+        try{
+            status = trace.begin(message);
+            
+            //로직 호출
+            T result = call();
+            
+            trace.end(status);
+            return result;
+        }catch(Exception e){
+            trace.exception(status, e);
+            throw e;
+            
+        }
+    }
+
+    protected abstract T call();
+}
