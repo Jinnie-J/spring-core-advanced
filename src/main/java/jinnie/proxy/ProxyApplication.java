8e694aa53cbda94e241a2b8530b305b8015ad91e
Jinnie-J <62706198+Jinnie-J@users.noreply.github.com>
2022-09-20 23:35:49 +0900

    인터페이스 기반 프록시와 클래스 기반 프록시

--- src/main/java/jinnie/proxy/ProxyApplication.java
+++ src/main/java/jinnie/proxy/ProxyApplication.java
@@ -4,6 +4,7 @@ import jinnie.advanced.trace.logtrace.LogTrace;
 import jinnie.advanced.trace.logtrace.ThreadLocalLogTrace;
 import jinnie.proxy.config.AppV1Config;
 import jinnie.proxy.config.AppV2Config;
+import jinnie.proxy.config.v1_proxy.ConcreteProxyConfig;
 import jinnie.proxy.config.v1_proxy.InterfaceProxyConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -11,7 +12,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
 
 //@Import({AppV1Config.class, AppV2Config.class})
-@Import(InterfaceProxyConfig.class)
+//@Import(InterfaceProxyConfig.class)
+@Import(ConcreteProxyConfig.class)
 @SpringBootApplication(scanBasePackages = "jinnie.proxy.app") //주의
 public class ProxyApplication {
 
---
+++ src/main/java/jinnie/proxy/config/v1_proxy/concrete_proxy/OrderControllerConcreteProxy.java
@@ -0,0 +1,37 @@
+package jinnie.proxy.config.v1_proxy.concrete_proxy;
+
+import jinnie.advanced.trace.TraceStatus;
+import jinnie.advanced.trace.logtrace.LogTrace;
+import jinnie.proxy.app.v2.OrderControllerV2;
+
+public class OrderControllerConcreteProxy extends OrderControllerV2 {
+
+    private final OrderControllerV2 target;
+    private final LogTrace logTrace;
+
+    public OrderControllerConcreteProxy(OrderControllerV2 target, LogTrace logTrace) {
+        super(null);
+        this.target = target;
+        this.logTrace =logTrace;
+    }
+
+    @Override
+    public String request(String itemId) {
+        TraceStatus status = null;
+        try {
+            status = logTrace.begin("OrderController.request()");
+            //target 호출
+            String result = target.request(itemId);
+            logTrace.end(status);
+            return result;
+        } catch (Exception e) {
+            logTrace.exception(status, e);
+            throw e;
+        }
+    }
+
+    @Override
+    public String noLog() {
+        return target.noLog();
+    }
+}
